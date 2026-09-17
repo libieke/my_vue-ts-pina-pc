73825f3c91ca5d8a1357d80104b34537d14e3b45
libieke <105412392+libieke@users.noreply.github.com>
2026-09-17 15:45:42 +0800

    fix: set Vue Router base for GitHub Pages

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -53,7 +53,7 @@ export const constantRoutes = [
 
 // 添加动态路由
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: constantRoutes,
   scrollBehavior() {
     return {
