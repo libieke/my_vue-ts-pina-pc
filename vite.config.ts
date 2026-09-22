d6b5105c2f3d88e0abe5b7f9b0c5cef4298ec525
libieke <libieke@users.noreply.github.com>
2026-09-22 14:47:46 +0800

    clearnmoreCode

--- vite.config.ts
+++ vite.config.ts
@@ -1,74 +1,48 @@
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
-
 import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
-
 import AutoImport from "unplugin-auto-import/vite";
-
 import Components from "unplugin-vue-components/vite";
-
-import {
-  ElementPlusResolver,
-  AntDesignVueResolver,
-} from "unplugin-vue-components/resolvers";
-
-export default defineConfig(({ mode }) => {
+import { ElementPlusResolver, AntDesignVueResolver } from "unplugin-vue-components/resolvers";
+export default defineConfig(({
+  mode
+}) => {
   const env = loadEnv(mode, process.cwd());
-
   return {
     base: env.VITE_BASE_URL || "/",
-
-    plugins: [
-      vue(),
-
-      createSvgIconsPlugin({
-        iconDirs: [resolve(process.cwd(), "src/icons")],
-        symbolId: "icon-[dir]-[name]",
-      }),
-
-      AutoImport({
-        imports: ["vue", "vue-router"],
-        dts: "src/auto-import.d.ts",
-        resolvers: [ElementPlusResolver(), AntDesignVueResolver()],
-      }),
-
-      Components({
-        resolvers: [
-          ElementPlusResolver({
-            importStyle: "sass",
-          }),
-          AntDesignVueResolver(),
-        ],
-      }),
-    ],
-
+    plugins: [vue(), createSvgIconsPlugin({
+      iconDirs: [resolve(process.cwd(), "src/icons")],
+      symbolId: "icon-[dir]-[name]"
+    }), AutoImport({
+      imports: ["vue", "vue-router"],
+      dts: "src/auto-import.d.ts",
+      resolvers: [ElementPlusResolver(), AntDesignVueResolver()]
+    }), Components({
+      resolvers: [ElementPlusResolver({
+        importStyle: "sass"
+      }), AntDesignVueResolver()]
+    })],
     build: {
       outDir: "me-test",
-
       assetsDir: "static",
-
-      target: "esnext",
+      target: "esnext"
     },
-
     server: {
       port: 5173,
-
       proxy: {
         "/api": {
           target: "http://192.168.1.104:8999/",
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ""),
-        },
-      },
+          rewrite: path => path.replace(/^\/api/, "")
+        }
+      }
     },
-
     resolve: {
       alias: {
-        "@": resolve(__dirname, "./src"),
+        "@": resolve(__dirname, "./src")
       },
-
-      extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue", "png"],
-    },
+      extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue", "png"]
+    }
   };
 });
